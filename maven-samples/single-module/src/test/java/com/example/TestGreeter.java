ab9ac98228577480fcdd122ff91dddffd699d732
Linh Pham Tung <phamtung.linh@vietcredit.com.vn>
2022-07-25 00:11:50 +0700

    linhpt

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,13 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
-    @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "This is world linhpt!";
+    String someone = "this World aaaaalinhpt123";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
